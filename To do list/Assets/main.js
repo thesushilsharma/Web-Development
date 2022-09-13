a661f7ac69510d8f0f20b7068ea174fc8bd7963a
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-09-13 05:36:00 +0400

    death note

--- To do list/Assets/main.js
+++ To do list/Assets/main.js
@@ -1,69 +1,82 @@
-// create database connection
-var Database_Name = 'mydb';
+var Database_Name = 'keeper';
 var Version = '1.0';
 var Text_Description = 'Sushil Sharma';
 var Database_Size = 5 * 1024 * 1024;
-var db = openDatabase(Database_Name, Version, Text_Description, Database_Size);
+var mydb = openDatabase(Database_Name, Version, Text_Description, Database_Size);
 
 
-// create a database
-db.transaction(function (tx) {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS NOTES(topic TEXT, note VARCHAR)");
-})
-
-// display if anything available in database
-db.transaction(function (tx) {
-    tx.executeSql('SELECT * FROM NOTES', [], function (tx, results) {
-        var len = results.rows.length, i;
-
-        for (i = 0; i < len; i++) {
-            msg = "<p><b>" + results.rows.item(i).topic + "</b><br>" + results.rows.item(i).note + "</p>";
-            document.querySelector('#OutputSection').innerHTML += msg;
-        }
-    }, null);
+//create the  table using SQL for the database using a transaction
+mydb.transaction(function (tx) {
+    tx.executeSql("CREATE TABLE IF NOT EXISTS NOTES(id INTEGER PRIMARY KEY, topic TEXT, note VARCHAR)");
 });
 
-// If user adds a note, add it to the websql
+//Function to show elements from websql
+function updateNotes(tx, results) {
+
+    var node = document.getElementById("savednotes");
+    
+    node.innerHTML = "";
+    var i;
+    var len = results.rows.length;
+    //Iterate through the results
+    for (i = 0; i < len; i++) {
+        //Get the current row from database
+        var row = results.rows.item(i);
+
+        node.innerHTML += "<li><p><b>" + row.topic + "</b><br><q>" + row.note + "</q></p><br><button onclick='deleteNote(" + row.id + ");'>Delete Draft</button></li>";
+    }
+}
+
+//function to display the list of notes in the database
+function outputNotes() {
+
+    mydb.transaction(function (tx) {
+        tx.executeSql("SELECT * FROM NOTES", [], updateNotes);
+    });
+
+}
+
+//If user adds a note, add it to the websql
 function createTicket() {
+
     var topic = document.getElementById("topic").value;
     var note = document.getElementById("note").value;
     var validation = /^([^@\s]+@[^@\s]+\.[^@\s]+)$/;
 
     if (validation.test(topic)) {
-
-        db.transaction(function (tx) {
-            tx.executeSql(`INSERT INTO NOTES VALUES("${topic}", "${note}")`);
+        mydb.transaction(function (tx) {
+            tx.executeSql(`INSERT INTO NOTES (topic, note) VALUES("${topic}", "${note}")`);
+            outputNotes();
         });
-        createNewNote(topic, note);
+    } else {
+        alert("Invalid email");
     }
-    else
-        alert("Incorrect email format.");
 }
 
-// Function to show elements from websql
-function createNewNote(topic, note) {
+//Function to delete a note
+function deleteNote(id) {
 
-    var paragraph = document.createElement("p");
-    paragraph.innerHTML = `${topic}<br>${note}<br><br>`;
-    document.getElementById("OutputSection").appendChild(paragraph);
-}
-
-// Function to delete a note
-function clearDatabase() {
-    document.getElementById("OutputSection").innerHTML = `Empty! Use "Add a Note" section above to add notes.`;
-    db.transaction(function (tx) {
-        tx.executeSql("DROP TABLE IF EXISTS NOTES");
-        tx.executeSql("CREATE TABLE IF NOT EXISTS NOTES(topic TEXT, note VARCHAR)");
+    mydb.transaction(function (tx) {
+        tx.executeSql("DELETE FROM NOTES WHERE id=?", [id], outputNotes);
     });
 
 }
 
+//Function to delete a table
+function clearDatabase() {
 
+    document.getElementById("savednotes").innerHTML = `Empty! Use "Add a Note" section above to add notes.`;
+    mydb.transaction(function (tx) {
+        tx.executeSql("DROP TABLE IF EXISTS NOTES");
+        tx.executeSql("CREATE TABLE IF NOT EXISTS NOTES(id INTEGER PRIMARY KEY, topic TEXT, note VARCHAR)");
+    });
+
+}
+outputNotes()
 
 /*
 Further Features:
 1. Add Title
 2. Mark a note as Important
-3. Delete Individual note
-4. Separate notes by user
+3. Separate notes by user
 */
