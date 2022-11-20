1a62b40600db8bf46c224ac309066ac80f6a5af2
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-11-21 01:41:58 +0400

    adding

---
+++ Expense-Tracker/assets/main.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const credit = document.getElementById('credit');
+const debit = document.getElementById('debit');
+const activity = document.getElementById('activity');
+const form = document.getElementById('form');
+const title = document.getElementById('title');
+const amount = document.getElementById('amount');
