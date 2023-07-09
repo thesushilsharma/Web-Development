c34fcb586d30e9e5ac5e77a10c02d9f2313ee3f3
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-07-09 22:28:07 +0400

    updating desgin

--- Drink-Water-Reminder/assets/cal.js
+++ Drink-Water-Reminder/assets/cal.js
@@ -1,27 +1,44 @@
-const WEIGHT = parseFloat(prompt("What's your Weight (KG)? "));
-const AGE = parseFloat(prompt("What's your Age? "));
-
 const OUNCE_PER_LITER = 33.814;
-
+const OUNCE_PER_GRAM = 28.3;
 function glassesPerDay(age, weight) {
-    let ratioPerAge = 0;
-    if (age < 30) {
-        ratioPerAge = 40;
-    }
-    else if (age <= 30 || age >= 55) {
-        ratioPerAge = 35;
-    }
-    else {
-        ratioPerAge = 30;
-    }
-    const ounces = (weight * ratioPerAge) / 28.3;
-    const cups = ounces / 8;
-    const l = ounces / OUNCE_PER_LITER;
+  let ratioPerAge = 0;
+  if (age < 30) {
+    ratioPerAge = 40;
+  } else if (age >= 30 && age < 55) {
+    ratioPerAge = 35;
+  } else {
+    ratioPerAge = 30;
+  }
+  const ounces = (weight * ratioPerAge) / OUNCE_PER_GRAM;
+  const cups = ounces / 8; //US cup (236.6mL)
+  const liters = ounces / OUNCE_PER_LITER;
 
-    return parseFloat(ounces).toFixed(2) + ' ozs' + "<br>" + parseFloat(l).toFixed(3) + ' L' + "<br>" + parseInt(cups) + ' cups';
+  return {
+    ounces: ounces.toFixed(2),
+    liters: liters.toFixed(3),
+    cups: parseInt(cups),
+  };
 }
 
-function main() {
-    document.write('<div id="output">'+glassesPerDay(AGE, WEIGHT)+'</div');
+function calculateWaterIntake() {
+  const weightInput = document.getElementById('weightInput');
+  const ageInput = document.getElementById('ageInput');
+  const weight = parseFloat(weightInput.value);
+  const age = parseFloat(ageInput.value);
+
+  const error = document.querySelector('.error');
+
+  if (isNaN(weight) || isNaN(age)) {
+    error.textContent = 'Please enter valid weight and age.';
+    return;
+  }
+  const result = glassesPerDay(age, weight);
+  const resultElement = document.getElementById('result');
+  error.style.display = 'none';
+  resultElement.innerHTML = `
+    <p class="text-lg font-bold">Water Intake:</p>
+    <p>${result.ounces} ozs</p>
+    <p>${result.liters} L</p>
+    <p>${result.cups} cups</p>
+  `;
 }
-main()
