0bce755129b3774c986d496ab792ee4dbf4c3d93
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-11-27 02:26:43 +0400

    updating and adding emoji

--- Forms/assets/script.js
+++ Forms/assets/script.js
@@ -19,41 +19,52 @@ async function init() {
     try {
         const data = await fetchData('assets/countries.json');
 
-        const dropdown = document.getElementById('countryDropdown');
-        const flagIconSpan = document.getElementById('flagIcon');
-        const phoneNumberInput = document.getElementById('phoneNumber');
-        const currencySelect = document.getElementById('currency_select');
-        const nationSelect = document.getElementById('nation_select');
+        const dropdown = document.getElementById("countryDropdown");
+        const phoneNumberInput = document.getElementById("phoneNumber");
+        const currencySelect = document.getElementById("currency_select");
+        const nationSelect = document.getElementById("nation_select");
 
-        data.forEach(country => {
-            const { country_code, country_name, dialling_code } = country;
-            const option = createDropdownOption(country_code, `${country_name} (${dialling_code})`, {
-                'country-name': country_name,
-                'flag-icon-class': country_code.toLowerCase(),
-                'dial-code': dialling_code,
-            });
+        data.forEach((country) => {
+            const { country_code, country_name, dialling_code, country_emoji } =
+                country;
+            const option = createDropdownOption(
+                country_code,
+                `${country_emoji} ${country_name} (${dialling_code})`,
+                {
+                    "country-name": country_name,
+                    "flag-icon-class": country_code.toLowerCase(),
+                    "dial-code": dialling_code,
+                }
+            );
             dropdown.add(option);
         });
 
-        data.forEach(currency => {
-            const option = createDropdownOption(currency.currency, currency.currency, {});
+        data.forEach((money) => {
+            const { currency, country_emoji } = money;
+            const option = createDropdownOption(
+                currency,
+                `${country_emoji} ${currency}`,
+                {}
+            );
             currencySelect.add(option);
         });
 
-        data.forEach(nationality => {
-            const option = createDropdownOption(nationality.country_name, nationality.country_name, {});
+        data.forEach((nationality) => {
+            const { country_name, country_emoji } = nationality;
+            const option = createDropdownOption(
+                country_name,
+                `${country_emoji} ${country_name}`,
+                {}
+            );
             nationSelect.add(option);
         });
 
-        dropdown.addEventListener('change', function () {
+        dropdown.addEventListener("change", function () {
             const selectedOption = this.options[this.selectedIndex];
-            const flagIconClass = selectedOption.getAttribute('data-flag-icon-class');
-            const flagName = selectedOption.getAttribute('data-country-name');
-            const dialCode = selectedOption.getAttribute('data-dial-code');
+            const dialCode = selectedOption.getAttribute("data-dial-code");
 
-            flagIconSpan.innerHTML = `<img src="https://flagcdn.com/${flagIconClass}.svg" class="inline-block w-7" alt="${flagName}"/>`;
             phoneNumberInput.value = `${dialCode}`; // Set input value to the dial code
-            console.log('Selected Country Code:', selectedOption.value);
+            console.log("Selected Country Code:", selectedOption.value);
         });
     } catch (error) {
         console.error('Error:', error);
